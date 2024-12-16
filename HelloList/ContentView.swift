317c0da48e242389ef1a8757f04e00767621c210
Willy <willy548798@gmail.com>
2024-12-16 16:30:53 +0800

    SwiftUI List.insetGrouped

--- HelloList/ContentView.swift
+++ HelloList/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
+            Section("Group 1") {
                 Text("Hello, world!")
                 Text("Hello, world!")
                 Text("Hello, world!")
+            }
+            Section("Group 2") {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
         }
+        .listStyle(.insetGrouped)
     }
 }
 
