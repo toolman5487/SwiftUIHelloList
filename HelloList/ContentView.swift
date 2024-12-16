ed2acd55ffe76e502bd3738bd64029d3a4c2550b
Willy <willy548798@gmail.com>
2024-12-16 16:17:54 +0800

    Initial Commit

---
+++ HelloList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloList
+//
+//  Created by Willy Hsu on 2024/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloList/HelloListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloListApp.swift
+//  HelloList
+//
+//  Created by Willy Hsu on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct HelloListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
